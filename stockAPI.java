91ec24f8c3baa4f162495096ee002ec5df4d4710
WarMachine108 <agarwal.tanmay2007@gmail.com>
2025-11-16 14:46:21 +0530

    changed to yahoo finance thingy

--- stockAPI.java
+++ stockAPI.java
@@ -2,32 +2,41 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import com.google.gson.*;
 
-//currently gives out current price of apple's stock, update later
-public class stockAPI { 
+class yahoofin{
 
     private static final HttpClient client = HttpClient.newHttpClient();
-    private static final String apiKey = "3debf4dac7cb46daa8351151d229dda7";
 
-    //change later to return price, right now its void prints the data in json -> parse it and return a double.
-    public static double getPrice(String symbol) {
+    public static String getRawData(String symbol, String interval, String range) {
         try {
-            HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://api.twelvedata.com/price?symbol=" + symbol + "&apikey=" + apiKey))
-                .build();
+            String url = "https://query1.finance.yahoo.com/v8/finance/chart/" +
+                         symbol + "?interval=" + interval + "&range=" + range;
 
-            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-            JsonObject json = JsonParser.parseString(response.body()).getAsJsonObject();
-            return json.get("price").getAsDouble();
-        }
-        catch (Exception e) {
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(url))
+                    .header("User-Agent", "Mozilla/5.0")
+                    .build();
+
+            HttpResponse<String> response =
+                    client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            return response.body();
+
+        } catch (Exception e) {
             e.printStackTrace();
-            return -1;
+            return null;
         }
     }
+}
 
+public class stockAPI {
     public static void main(String[] args) {
-        System.out.println(getPrice("AAPL")); // test
+
+        String symbol = "%5ENSEI"; // NIFTY50
+
+        // Call stockAPI
+        String json = yahoofin.getRawData(symbol, "1d", "max");
+
+        System.out.println(json);
     }
 }
